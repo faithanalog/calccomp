5a89c3572a16701af406e61ee5b4c93df6b66820
unknownloner <unknownloner.online@gmail.com>
2014-10-04 00:25:58 -0400

    Working asm parser

--- Asm/Expr.hs
+++ Asm/Expr.hs
@@ -1,13 +1,168 @@
 module Asm.Expr where
 
-type Ident = String
-type Instr = String
-type Reg = String
+import Data.Char (toUpper)
+import Data.List (intercalate)
+
+data Register = IXH | IXL | IYH | IYL | A | B | C | D | E | H | L | BC | DE | HL | IX | IY | I | R deriving (Show,Ord,Eq,Read)
+
+data Instruction = EX | EXX | LD | LDD | LDDR | LDI | LDIR | POP | PUSH
+                 | ADC | ADD | CP | CPD | CPDR | CPI | CPIR | CPL | DAA
+                 | DEC | INC | NEG | SBC | SUB
+                 | AND | BIT | CCF | OR | RES | SCF | SET | XOR
+                 | RL | RLA | RLC | RLCA | RLD
+                 | RR | RRA | RRC | RRCA | RRD | SLA | SRA | SRL
+                 | CALL | DJNZ | JP | JR | NOP | RET | RETI | RETN | RST
+                 | DI | EI | HALT | IM | IN | IND | INI | INIR
+                 | OTDR | OTIR | OUT | OUTD | OUTI deriving (Eq,Show,Read)
+
+data Op = Add | Sub | Mul | Div | Lt | Gt | LShift | RShift | Mod deriving (Eq)
+
+data Condition = CondZ | CondNZ | CondC | CondNC | CondPO | CondPE | CondP | CondM | CondNOCOND deriving (Eq,Read)
+
+instance Show Op where
+    show Add = "+"
+    show Sub = "-"
+    show Mul = "*"
+    show Div = "/"
+    show Lt = "<"
+    show Gt = ">"
+    show Mod = "%"
+    show LShift = "<<"
+    show RShift = ">>"
+
+instance Show Condition where
+    show CondZ = "z"
+    show CondNZ = "nz"
+    show CondC = "c"
+    show CondNC = "nc"
+    show CondPO = "po"
+    show CondPE = "pe"
+    show CondP = "p"
+    show CondM = "m"
 
 data Expr = Label String
-          | Instr String [Expr]
+          | LabelDef String
+          | Instr Instruction [Expr]
+          | Cond Condition
           | Directive String [Expr]
-          | Reg String
+          | Reg Register
+          | RegIndir Register
+          | RegIndex Register Expr
+          | AddrIndir Expr
           | Num Integer
           | String String
-          deriving (Eq,Show)
+          | Binop Op Expr Expr
+          | Parens Expr
+          deriving (Eq)
+
+instance Show Expr where
+    show (Label lbl) = lbl
+    show (LabelDef lbl) = lbl ++ ":"
+    show (Instr ins xprs) = show ins ++ " " ++ (intercalate ", " $ map show xprs)
+    show (Cond cond) = show cond
+
+    show (Directive "define" xprs) = show (head xprs) ++ " = " ++ show (xprs !! 1)
+    show (Directive dir xprs) = "." ++ dir ++ " " ++ (intercalate ", " $ map show xprs)
+
+    show (Reg reg) = show reg
+    show (RegIndir reg) = "(" ++ show reg ++ ")"
+    show (RegIndex reg xpr) = "(" ++ show reg ++ " + " ++ show xpr ++ ")"
+    show (AddrIndir xpr) = "(" ++ show xpr ++ ")"
+    show (Num x) = show x
+    show (String str) = show str
+    show (Binop op lft rt) = showOpArg lft ++ " " ++ show op ++ " " ++ showOpArg rt
+    show (Parens xpr) = "(" ++ show xpr ++ ")"
+
+-- Top level binary ops should have no parentheses, nested ones should
+showOpArg :: Expr -> String
+showOpArg bop@(Binop {}) = "(" ++ show bop ++ ")"
+showOpArg x = show x
+
+getInstr :: String -> Instruction
+getInstr i = read (map toUpper i)
+
+maybeInstr :: String -> Maybe Instruction
+maybeInstr i
+    | null res = Nothing
+    | otherwise = Just $ fst . head $ res
+    where res = reads (map toUpper i)
+
+maybeReg :: String -> Maybe Register
+maybeReg r
+    | null res = Nothing
+    | otherwise = Just $ fst . head $ res
+    where res = reads (map toUpper r)
+
+getReg :: String -> Register
+getReg r = read (map toUpper r)
+
+getCond :: String -> Condition
+getCond c = read $ "Cond" ++ map toUpper c
+
+numArgs :: Instruction -> Int
+numArgs EX = 2
+numArgs EXX = 0
+numArgs LD = 2
+numArgs LDD = 0
+numArgs LDDR = 0
+numArgs LDI = 0
+numArgs LDIR = 0
+numArgs POP = 1
+numArgs PUSH = 1
+numArgs ADC = 2
+numArgs ADD = 2
+numArgs CP = 1
+numArgs CPD = 0
+numArgs CPDR = 0
+numArgs CPI = 0
+numArgs CPIR = 0
+numArgs CPL = 0
+numArgs DAA = 0
+numArgs DEC = 1
+numArgs INC = 1
+numArgs NEG = 0
+numArgs SBC = 2
+numArgs SUB = 1
+numArgs AND = 1
+numArgs BIT = 2
+numArgs CCF = 0
+numArgs OR = 1
+numArgs RES = 2
+numArgs SCF = 0
+numArgs SET = 2
+numArgs XOR = 1
+numArgs RL = 1
+numArgs RLA = 0
+numArgs RLC = 1
+numArgs RLCA = 0
+numArgs RLD = 0
+numArgs RR = 1
+numArgs RRA = 0
+numArgs RRC = 1
+numArgs RRCA = 0
+numArgs RRD = 0
+numArgs SLA = 1
+numArgs SRA = 1
+numArgs SRL = 1
+numArgs CALL = 2
+numArgs DJNZ = 1
+numArgs JP = 2
+numArgs JR = 2
+numArgs NOP = 0
+numArgs RET = 1
+numArgs RETI = 0
+numArgs RETN = 0
+numArgs RST = 1
+numArgs DI = 0
+numArgs EI = 0
+numArgs HALT = 0
+numArgs IM = 1
+numArgs IN = 2
+numArgs IND = 0
+numArgs INI = 0
+numArgs INIR = 0
+numArgs OTDR = 0
+numArgs OTIR = 0
+numArgs OUT = 2
+numArgs OUTD = 0
+numArgs OUTI = 0
--- Asm/Parser.hs
+++ Asm/Parser.hs
@@ -1,7 +1,7 @@
-module Asm.Parser (parseExpr) where
+module Asm.Parser (parseExpr, printTree) where
 
 import Asm.Expr
-import Text.Parsec
+import Text.Parsec hiding (space, spaces)
 import Text.Parsec.Expr
 import Text.Parsec.String (Parser)
 import Text.Parsec.Language (emptyDef)
@@ -10,7 +10,9 @@ import Control.Monad
 import Control.Applicative ((<$>), (<*>))
 import System.Environment
 import Numeric
-
+import Data.List (intercalate)
+import Data.Maybe (catMaybes)
+import Data.Char (ord, toUpper)
 
 
 -- Lexer
@@ -18,135 +20,328 @@ import Numeric
 lexer :: Token.TokenParser ()
 lexer = Token.makeTokenParser style
   where style = emptyDef {
-          Token.reservedOpNames = ["+","*","-","="],
+          Token.opStart = oneOf "+-*/=<>",
+          Token.opLetter = oneOf "<>=",
           Token.reservedNames = [".org", ".db", ".dw"],
           Token.commentLine = ";",
           Token.identStart = letter <|> char '_',
-          Token.identLetter = alphaNum <|> char '_'
+          Token.identLetter = alphaNum <|> char '_',
+          Token.caseSensitive = False
         }
 
-decimal :: Parser Integer
-decimal = Token.decimal lexer
+parseBin :: String -> Integer
+parseBin = foldl (\l r -> l * 2 + (if r == '1' then 1 else 0)) 0
+
+binPrefix :: Parser Integer
+binPrefix = do
+    char '%'
+    str <- many1 $ oneOf "01"
+    return $ parseBin str
+
+binSuffix :: Parser Integer
+binSuffix = do
+    str <- many1 $ oneOf "01"
+    oneOf "Bb"
+    return $ parseBin str
 
 binary :: Parser Integer
-binary = do
-    char '%'
-    str <- many $ oneOf "01"
-    return $ foldl (\l r -> l * 2 + (if r == '0' then 0 else 1)) 0 str
+binary = binPrefix <|> binSuffix
 
-hexadecimal :: Parser Integer
-hexadecimal = do
+hexPrefix :: Parser Integer
+hexPrefix = do
     char '$'
-    str <- many hexDigit
+    str <- many1 hexDigit
     return $ fst . head . readHex $ str
 
+hexSuffix :: Parser Integer
+hexSuffix = do
+    str <- many1 hexDigit
+    oneOf "Hh"
+    return $ fst . head . readHex $ str
+
+hexadecimal :: Parser Integer
+hexadecimal = hexPrefix <|> hexSuffix
+
+decimal :: Parser Integer
+decimal = do
+    str <- many1 digit
+    return $ read str
+
+charNum :: Parser Integer
+charNum = do
+    char '\''
+    c <- anyChar
+    char '\''
+    return $ toInteger (ord c)
+
 integer :: Parser Integer
-integer = do
+integer = lexeme $ do
     sign <- option '+' (oneOf "+-")
-    num <- decimal <|> binary <|> hexadecimal
+    num <- try binary <|> try hexadecimal <|> try decimal <|> try charNum
+    notFollowedBy lblChar
     return $ if sign == '+' then num else -num
 
+space :: Parser Char
+space = char ' '
+
+spaces :: Parser String
+spaces = many1 space
+
+comment :: Parser String
+comment = do
+    char ';'
+    many (try $ noneOf "\n")
+
+whiteSpace :: Parser ()
+whiteSpace = skipMany (spaces <|> comment)
+
+lexeme :: Parser a -> Parser a
+lexeme p = do
+    x <- p
+    whiteSpace
+    return x
+
+
+stringNoCase :: String -> Parser String
+stringNoCase "" = return ""
+stringNoCase (s:str) = do
+    l <- satisfy (\c -> toUpper c == toUpper s)
+    r <- stringNoCase str
+    return $ l:r
+
 symbol :: String -> Parser String
-symbol = Token.symbol lexer
+symbol = lexeme . stringNoCase
 
 parens :: Parser a -> Parser a
-parens = Token.parens lexer
+parens p = do
+    symbol "("
+    x <- p
+    symbol ")"
+    return x
 
-reserved :: String -> Parser ()
-reserved = Token.reserved lexer
+braces :: Parser a -> Parser a
+braces p = do
+    symbol "{"
+    x <- p
+    symbol "}"
+    return x
 
-reservedOp :: String -> Parser ()
-reservedOp = Token.reservedOp lexer
+semi :: Parser String
+semi = symbol ";"
+
+comma :: Parser String
+comma = symbol ","
+
+operator :: Parser String
+operator = lexeme $ do
+    op <- oneOf "+-*/><%"
+    return [op]
 
 identifier :: Parser String
-identifier = Token.identifier lexer
+identifier = lexeme $ do
+    x <- letter <|> char '_'
+    xs <- many (alphaNum <|> char '_')
+    return $ x:xs
 
 commaSep :: Parser a -> Parser [a]
-commaSep = Token.commaSep lexer
+commaSep p = lexeme $ p `sepBy` comma
+
+semiSep :: Parser a -> Parser [a]
+semiSep p = lexeme $ p `sepBy` semi
+
+stringEscape :: Parser Char
+stringEscape = char '\'' >> oneOf "\\\""
 
 stringLiteral :: Parser String
-stringLiteral = Token.stringLiteral lexer
+stringLiteral = lexeme $ do
+    char '"'
+    str <- many (stringEscape <|> noneOf "\"")
+    char '"'
+    return str
+
+singleton :: Parser a -> Parser [a]
+singleton p = do
+    x <- p
+    return [x]
+
+lblChar :: Parser Char
+lblChar = alphaNum <|> char '_'
+
+lblIdentifier :: Parser String
+lblIdentifier = lexeme $ many1 lblChar
 
 -- Parser
 asmlabel :: Parser Expr
-asmlabel = do
-    name <- identifier
+asmlabel = lexeme $ do
+    name <- lblIdentifier
     char ':'
-    return $ Label name
+    return $ LabelDef name
+
+condition :: Parser Expr
+condition = do
+    condName <- choice [
+                try $ symbol "z",
+                try $ symbol "nz",
+                try $ symbol "c",
+                try $ symbol "nc",
+                try $ symbol "po",
+                try $ symbol "pe",
+                try $ symbol "p",
+                try $ symbol "m"
+            ]
+    return $ Cond $ getCond condName
 
 instr :: Parser Expr
 instr = do
     ident <- identifier
-    args <- commaSep argExpr
-    return $ Instr ident args
+    instr <- case maybeInstr ident of
+            Nothing -> parserZero
+            Just x -> return x
+    let jpCond = do
+            cond <- option [] $ try $ do
+                x <- condition
+                symbol ","
+                return [x]
+            arg <- argExpr
+            return $ cond ++ [arg]
+    args <- case instr of
+            CALL -> jpCond
+            JR -> jpCond
+            JP -> jpCond
+            RET -> option [] $ try $ singleton condition
+            instr -> case numArgs instr of
+                0 -> return []
+                1 -> singleton argExpr
+                2 -> do
+                    xpr1 <- argExpr
+                    symbol ","
+                    xpr2 <- argExpr
+                    return [xpr1, xpr2]
+    return $ Instr instr args
 
 num :: Parser Expr
 num = Num `fmap` integer
 
 labelref :: Parser Expr
-labelref = Label `fmap` identifier
+labelref = Label `fmap` (lblIdentifier <|> do x <- char '$'; return [x])
+
+constAssign :: Parser Expr
+constAssign = do
+    name <- lblIdentifier
+    symbol "="
+    val <- argExpr
+    return $ Directive "define" [Label name, val]
 
 asmstring :: Parser Expr
 asmstring = String `fmap` stringLiteral
 
 register :: Parser Expr
 register = do
-    regName <- choice [
-              symbol "a",
-              symbol "b",
-              symbol "c",
-              symbol "d",
-              symbol "e",
-              symbol "h",
-              symbol "l",
-              symbol "ixh",
-              symbol "ixl",
-              symbol "iyh",
-              symbol "iyl",
-              symbol "af",
-              symbol "bc",
-              symbol "de",
-              symbol "hl",
-              symbol "ix",
-              symbol "iy",
-              symbol "r",
-              symbol "i",
-              symbol "sp",
-              symbol "af'"
-           ]
-    return $ Reg regName
+    ident <- identifier
+    reg <- case maybeReg ident of
+                Nothing -> parserZero
+                Just r -> return r
+    return $ Reg reg
 
-indirectRegister :: Parser Expr
-indirectRegister = do
-    char '('
-    regName <- choice [
+regIndirect :: Parser Expr
+regIndirect = do
+    regName <- parens $ choice [
                 symbol "hl",
                 symbol "de",
                 symbol "bc",
-                symbol "ix",
-                symbol "iy"
+                try $ symbol "ix",
+                symbol "iy",
+                symbol "c"
             ]
-    char ')'
-    return $ Reg $ '(':regName ++ ")"
+    return $ RegIndir $ getReg regName
+
+regIndex :: Parser Expr
+regIndex = do
+    (regName, offs) <- parens $ do
+        reg <- choice [try $ symbol "ix", symbol "iy"]
+        offs <- labelref <|> num
+        return (reg, offs)
+    return $ RegIndex (getReg regName) offs
+
+addrIndirect :: Parser Expr
+addrIndirect = do
+    addr <- parens mathExpr
+    return $ AddrIndir addr
 
 directive :: Parser Expr
 directive = do
     oneOf "#."
     ident <- identifier
-    args <- many (noneOf "\n") >>= commaSep (choice [num, asmstring, labelref])
+    args <- commaSep directiveArg
     return $ Directive ident args
 
-argExpr :: Parser Expr
-argExpr = register
-       <|> try indirectRegister
-       <|> liftM Label identifier
-       <|> liftM Num integer
+mathOp :: Parser (Expr -> Expr -> Expr)
+mathOp = do
+    op <- operator
+    return $ case op of
+                "+" -> Binop Add
+                "-" -> Binop Sub
+                "*" -> Binop Mul
+                "/" -> Binop Div
+                "<" -> Binop Lt
+                ">" -> Binop Gt
+                "%" -> Binop Mod
+                "<<" -> Binop LShift
+                ">>" -> Binop RShift
 
-expr :: Parser Expr
-expr = directive <|> try asmlabel <|> instr
+parensExpr :: Parser Expr -> Parser Expr
+parensExpr p = do
+    xpr <- parens p
+    return $ Parens xpr
+
+binOp :: Parser Expr
+binOp = chainl1 (try num <|> try labelref <|> try (parensExpr mathExpr)) mathOp
+
+binOp' :: Parser Expr
+binOp' = do
+    lft <- labelref
+    op <- mathOp
+    rt <- labelref
+    return $ op lft rt
+
+mathExpr :: Parser Expr
+mathExpr = try binOp
+        <|> try num
+        <|> labelref
+
+directiveArg :: Parser Expr
+directiveArg = try mathExpr
+            <|> asmstring
+            <|> parens directiveArg
+
+argExpr :: Parser Expr
+argExpr = try register
+       <|> try regIndirect
+       <|> try regIndex
+       <|> try binOp
+
+
+statement :: Parser Expr
+statement = try directive <|> try constAssign <|> try asmlabel <|> instr
+
+parseStatements :: Parser [Expr]
+parseStatements = do
+    let parseLine = whiteSpace >> optionMaybe (try statement)
+    stmnts <- parseLine `sepBy` many1 newline
+    return $ catMaybes stmnts
+
+indirPass :: [Expr] -> [Expr]
+indirPass = map conv
+    where conv (Instr i args) = Instr i (map convParens args)
+          conv x = x
+          convParens (Parens xpr) = AddrIndir xpr
+          convParens x = x
 
 parseExpr :: String -> [Expr]
 parseExpr t =
-  case parse (many1 expr) "stdin" t of
+  case parse parseStatements "stdin" t of
     Left err -> error (show err)
-    Right ast -> ast
+    Right ast -> indirPass ast
+
+printTree :: [Expr] -> String
+printTree xprs = unlines (map show xprs)
